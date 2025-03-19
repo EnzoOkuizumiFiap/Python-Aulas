48884456beba03af34017e3ad2d821f98ba4d5c1
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-03-19 15:47:31 -0300

    Add files via upload
    Aula-4 com exercícios feitos

--- aula-4.py
+++ aula-4.py
@@ -1,3 +1,4 @@
+import math
 #Exercício 08 - Calcular área de um círculo: Peça ao usuário por input o raio de um círculo. Calcule e mostre sua área.
 PI = 3.14 #Valor Constante
 
@@ -28,7 +29,6 @@ print("Número de ocorrências da letra 'e':", ocorrencias_e)
 
 print("\n")
 
-##Exercício 10 - Calcular distância de um ponto até a origem.
 '''
 Calcular a distância de um ponto até a origem (coordenada (0, 0) do plano). Seu programa irá:
 
@@ -45,7 +45,48 @@ BÔNUS (a): calcular distância entre dois pontos distintos
 BÔNUS (b): calcular distância entre dois pontos em 3D (isto é, pontos com coordenadas (x, y, z))
 '''
 
-X = int(input("Coordenada X:"))
-Y = int(input("Coordenada Y:"))
+##Exercício 10 - Calcular distância de um ponto até a origem.
+
+# Ler as coordenadas x e y de um ponto
+print("Calculando a distância de um ponto até a origem\n")
+
+x = float(input("Diga a coordenada X:"))
+y = float(input("Diga a coordenada Y:"))
+
+#sqrt é √ (raiz) / (Potenciação > Soma) > Raiz
+distancia = math.sqrt(x**2 + y**2)
+
+print(f"Distância: {distancia:.2f}")
 
 
+
+#Leitura dos valores para calcular a distância entre dois pontos no plano 2D
+print("\nCalculando a distância entre 2 pontos no plano Cartesiano (2D)\n")
+x_1 = float(input("Diga a coordenada do X_1:"))
+y_1 = float(input("Diga a coordenada do Y_1:"))
+
+x_2 = float(input("Diga a coordenada do X_2:"))
+y_2 = float(input("Diga a coordenada do Y_2:"))
+
+distancia_2d = math.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
+
+print(f"A distância entre os dois pontos é de {distancia_2d}")
+
+
+
+#Calculando a distância entre dois pontos no espaço 3D
+print("\nCalculando a distância entre dois pontos no espaço Tridimensional")
+
+print("\nDiga as coordenadas do seu PRIMEIRO PONTO")
+x_01 = float(input("Diga a coordenada X_01: "))
+y_01 = float(input("Diga a coordenada Y_01: "))
+z_01 = float(input("Diga a coordenada Y_01: "))
+
+print("\nDiga as coordenadas do seu SEGUNDO PONTO")
+x_02 = float(input("Diga a coordenada X_02: "))
+y_02 = float(input("Diga a coordenada Y_02: "))
+z_02 = float(input("Diga a coordenada Z_02: "))
+
+distancia_3d = math.sqrt((x_02 - x_01) ** 2 + (y_02 - y_01) ** 2 + (z_02 - z_01) ** 2)
+
+print(f"A distância entre os pontos no plano 3D é {distancia_3d:.2f}")
