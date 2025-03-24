4c3c85ef904f34138fd30e3d5c1d2f654b8f127b
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-03-24 09:48:13 -0300

    Novos EX adicionados

--- aula-5.py
+++ aula-5.py
@@ -11,4 +11,37 @@ print('Estamos no segundo', data_atual.second)
 
 print('Estamos no dia', data_atual.day)
 print('Estamos no mês', data_atual.month)
+
 print('Estamos no ano', data_atual.year)
+
+#If e else com horários
+if data_atual.hour > 18 < 24:
+    print("Boa noite")
+if data_atual.hour > 12 < 18:
+    print("Boa tarde")
+else:
+    print("Bom dia")
+
+
+
+#Usando If e Else para selecionar opções de cálculo
+x = float(input("Digite um número:"))
+y = float(input("Digite outro número:"))
+
+print("Qual operação você deseja realizar: 1. Soma, 2. Multiplicação")
+
+opcao = int(input())
+if (opcao == 1):
+    print(x + y)
+else: 
+    print(x * y)
+
+
+
+#Se o usuário nasceu antes de 2001, diga que ele nasceu no século passado
+idade = int(input("Em que ano você nasceu?"))
+
+if idade < 2001:
+    print("Você nasceu no século 20")
+else:
+    print("Você nasceu no século 21")
