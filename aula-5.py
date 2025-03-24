4925a760b787637a2650144ea465aee34b38d715
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-03-24 09:12:32 -0300

    Adicionando arquivos aula-5

--- aula-5.py
+++ aula-5.py
@@ -1,2 +1,14 @@
 #Aula 5 de Python - 24/03
 
+#Use o datetime para mostrar a data e horário completos
+import datetime
+
+data_atual = datetime.datetime.now()
+
+print('Estamos na hora', data_atual.hour)
+print('Estamos no minuto', data_atual.minute)
+print('Estamos no segundo', data_atual.second)
+
+print('Estamos no dia', data_atual.day)
+print('Estamos no mês', data_atual.month)
+print('Estamos no ano', data_atual.year)
