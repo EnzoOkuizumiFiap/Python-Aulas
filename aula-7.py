862e87b14412c0bc3f0b5fc8ce8c4a4d386ed3ce
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-04-02 11:53:30 -0300

    Add files via upload

---
+++ aula-7.py
@@ -0,0 +1,33 @@
+'''
+Operadores lógicos juntam duas (ou mais) expressões lógicas em um único resultado.
+
+Operadores:
+
+Operador "or" (Ou)
+-Fica verdadeiro contanto que uma das expressões seja verdadeira
+
+Operadores "and" (E)
+-Fica verdadeiro só se todas as expressões são verdadeiras
+
+Operador "not" (Não)
+-Inverte o resultado (transforma True em False e vice-versa)
+'''
+
+x = 3
+y = 5
+mensagem = 'oi'
+
+if x > 8 or y < 10:
+    print("Operador OR disse que está Ok")
+else: 
+    print("Operador OR disse que NAÕ está Ok")
+
+if x > 8 and y < 10:
+    print("Operador AND diz que está ok")
+else:
+    print("Operador AND diz que NÃO está Ok")
+
+if not x > 8:
+    print("X não é maior que 8")
+else: 
+    print("X é maior que 8")
